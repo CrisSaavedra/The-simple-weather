89e488f6a242852c3bd6a983432c2311058e9e23
CrisSaavedra <crisisaave@gmail.com>
2023-03-27 20:32:52 -0300

    clear list  when click in another component

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -4,13 +4,15 @@ import { useFetch } from "../../hooks/useFetch";
 
 export const SearchBar = ({ cities, setCities }) => {
 
-    const [machCities, setMachCities] = useState([]);
+
+
 
     const onSearch = async (e) => {
         const value = e.target.value.toLowerCase();
         clearSuggestions();
         onValue();
         mangerData(value);
+
 
         if (e.key === 'Enter') {
             let { main, weather } = await useFetch(value);
@@ -27,7 +29,6 @@ export const SearchBar = ({ cities, setCities }) => {
 
     const onValue = () => {
         const element = document.getElementById("search");
-
         if (element != null && element.value != '') {
             element.classList.remove("bg-search");
             return;
@@ -41,20 +42,28 @@ export const SearchBar = ({ cities, setCities }) => {
 
     const mangerData = (value) => {
         const list = document.getElementById("cities");
-        
+
         let sortedData = cities.sort();
         for (let i of sortedData) {
-             let copyI = i.replace(/\s+/g, '');
-             let copyVal = value.replace(/\s+/g, '');  
+            let copyI = i.replace(/\s+/g, '');
+            let copyVal = value.replace(/\s+/g, '');
             if (value != '' && copyI.includes(copyVal)) {
-                let listItem = document.createElement("li");
+                let listItem = document.createElement("a");
                 listItem.style.cursor = "pointer";
                 listItem.innerHTML = i;
+                listItem.className = 'pb-2 focus:'
+                listItem.onclick = e => onClickOption(e);
                 if (list) {
                     list.appendChild(listItem);
                 }
             }
         }
+    }
+
+    const onClickOption = (e) => {
+        const element = document.getElementById("search");
+        element.value = e.target.innerHTML;
+        clearSuggestions();
     }
 
     const clearSuggestions = () => {
@@ -67,28 +76,40 @@ export const SearchBar = ({ cities, setCities }) => {
         }
     }
 
-    // document.addEventListener('click', (e) => {
-    // const list = document.getElementById("cities");
-    // if (e.target != list) {
-    //     if (list) {
-    //         while (list.firstChild) {
-    //             list.removeChild(list.firstChild);
-    //         }
-    //     }
-    // }
-    // })
+
+
+    const onClickOutside = (e) => {
+        clearSuggestions();
+    }
+
+    useEffect(() => {
+        document.addEventListener('click', e => {
+            console.log(e.target.id)
+            if (!e.target.id === 'search' || e) {
+                clearSuggestions();
+            }
+        })
+
+    }, []);
 
 
 
     return (
-        <div className="text-neutral-400 text-4xl flex flex-col items-center pt-20 ">
-            <div className=" bg-neutral-800  xl:container flex flex-col items-center">
-                <input id="search" onKeyUp={e => onSearch(e)} type="search" className="bg-neutral-800 h-20 xl:w-1/2 bg-search bg-no-repeat bg-left bg-10% xl:bg-5% outline-none focus:outline-none focus:bg-none focus:scale-110  hover:border-neutral-700  border-b border-neutral-700 transition-all ease-out  duration-500" />
+        <div className="text-neutral-400 text-4xl flex flex-col items-center mt-20 group w-7/12 mx-auto">
+            <div className="z-20 bg-neutral-800  xl:container flex flex-col  items-center transition-all ease-out  duration-500">
+                <input id="search" onKeyUp={e => onSearch(e)} type="search" className="group bg-neutral-800 h-20 xl:w-1/2 bg-search bg-no-repeat bg-left bg-10% xl:bg-5% outline-none focus:outline-none focus:bg-none  hover:border-neutral-700  border-b border-neutral-700 
+                focus:scale-110 transition-all ease-out  duration-500s" />
             </div>
-            <div>
-                <ul id="cities" className="pt-4">
+            <div className="fixed pt-20 w-7/12 flex justify-center group-hover:scale-100 mt-1 transition-all ease-out duration-500">
+                <ul id="cities" className="sticky w-11/12 top-0 mt-1 max-h-40  overflow-hidden bg-neutral-900 bg-brightness-100 opacity-90 text-neutral-400 flex flex-col  transition-all ease-out  duration-500">
 
                 </ul>
+            </div>
+
+
+            <div>
+                <h2>este es otro componente</h2>
+                <a href="#" id="dos">tienefocus?</a>
             </div>
 
 
