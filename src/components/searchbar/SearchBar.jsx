b887d683c3bc62230fc0df9b446bbe3e3edc842a
CrisSaavedra <crisisaave@gmail.com>
2023-03-28 02:00:39 -0300

    disble search after search

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -4,40 +4,49 @@ import { useFetch } from "../../hooks/useFetch";
 
 export const SearchBar = ({ cities, setCities }) => {
 
-
+    const [doc, setDoc] = useState();
 
 
     const onSearch = async (e) => {
+
+        const search = document.getElementById("search");
         const value = e.target.value.toLowerCase();
         clearSuggestions();
-        onValue();
-        mangerData(value);
-
+        onValue(search);
 
         if (e.key === 'Enter') {
             let { main, weather } = await useFetch(value);
             if (main && weather) {
-                setCities([...cities, value]);
+                let city = cities.find(c => c.toLowerCase() === value);
+                if (!city) {
+                    setCities([...cities, value]);
+                }
+                clearSuggestions();
+                onValue();
+                
+
             } else {
                 console.log('city doesnt exist')
             }
-
-            setSearch('');
             e.target.value = '';
+            
+            search.setAttribute('disabled', true) // averiguar como hacer disable o algo parecido cuando termina de buscar y guardar todo
+            console.log(search.attributes)
+        }else{
+            mangerData(value);
         }
     }
 
-    const onValue = () => {
-        const element = document.getElementById("search");
+    const onValue = (element) => {
+     
         if (element != null && element.value != '') {
             element.classList.remove("bg-search");
-            return;
-        }
-
-        if (element != null && element.value === '') {
+            
+        }else if (element != null && element.value === '') {
             element.classList.add("bg-search");
-            return;
+           
         }
+       
     }
 
     const mangerData = (value) => {
@@ -51,7 +60,7 @@ export const SearchBar = ({ cities, setCities }) => {
                 let listItem = document.createElement("a");
                 listItem.style.cursor = "pointer";
                 listItem.innerHTML = i;
-                listItem.className = 'pb-2 focus:'
+                listItem.className = 'pb-2 pl-2'
                 listItem.onclick = e => onClickOption(e);
                 if (list) {
                     list.appendChild(listItem);
@@ -63,6 +72,7 @@ export const SearchBar = ({ cities, setCities }) => {
     const onClickOption = (e) => {
         const element = document.getElementById("search");
         element.value = e.target.innerHTML;
+        element.focus();
         clearSuggestions();
     }
 
@@ -76,43 +86,32 @@ export const SearchBar = ({ cities, setCities }) => {
         }
     }
 
-
-
-    const onClickOutside = (e) => {
-        clearSuggestions();
-    }
-
-    useEffect(() => {
+    const eventListener = () => {
         document.addEventListener('click', e => {
-            console.log(e.target.id)
             if (!e.target.id === 'search' || e) {
                 clearSuggestions();
+                onValue();
             }
         })
+    }
 
+
+
+    useEffect(() => {
+        eventListener();
     }, []);
 
 
 
     return (
-        <div className="text-neutral-400 text-4xl flex flex-col items-center mt-20 group w-7/12 mx-auto">
-            <div className="z-20 bg-neutral-800  xl:container flex flex-col  items-center transition-all ease-out  duration-500">
-                <input id="search" onKeyUp={e => onSearch(e)} type="search" className="group bg-neutral-800 h-20 xl:w-1/2 bg-search bg-no-repeat bg-left bg-10% xl:bg-5% outline-none focus:outline-none focus:bg-none  hover:border-neutral-700  border-b border-neutral-700 
-                focus:scale-110 transition-all ease-out  duration-500s" />
+        <div className="relative text-neutral-400 text-4xl flex flex-col items-center mt-20 group mx-auto">
+            <div className="z-20 bg-neutral-800 w-11/12 xl:w-10/12  xl:container flex flex-col  items-center transition-all ease-out  duration-500">
+                <input id="search" onKeyUp={e => onSearch(e)} type="search" className="group bg-neutral-800 h-20 w-9/12 xl:w-1/2 bg-search bg-no-repeat bg-left bg-10% xl:bg-5% outline-none focus:outline-none focus:bg-none  hover:border-neutral-700  border-b border-neutral-700 
+                focus:scale-110 transition-all ease-out  duration-500 " autoComplete="true" />
             </div>
-            <div className="fixed pt-20 w-7/12 flex justify-center group-hover:scale-100 mt-1 transition-all ease-out duration-500">
-                <ul id="cities" className="sticky w-11/12 top-0 mt-1 max-h-40  overflow-hidden bg-neutral-900 bg-brightness-100 opacity-90 text-neutral-400 flex flex-col  transition-all ease-out  duration-500">
-
-                </ul>
+            <div className="absolute pt-20 w-11/12 xl:w-10/12 flex justify-center  mt-1 transition-all ease-out duration-500">
+                <ul id="cities" className="sticky w-9/12 scale-110 xl:w-1/2 top-0 mt-1 max-h-40  overflow-hidden bg-neutral-900 bg-brightness-100 opacity-80 text-neutral-400 flex flex-col overflow-y-scroll "></ul>
             </div>
-
-
-            <div>
-                <h2>este es otro componente</h2>
-                <a href="#" id="dos">tienefocus?</a>
-            </div>
-
-
         </div>
     )
 }
